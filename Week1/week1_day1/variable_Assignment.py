d0671191bcf3036330015dbc3822ebb80b943fde
aluong15 <l.aurora15@gmail.com>
2023-10-22 19:14:53 -0500

    first commit

---
+++ Week1/week1_day1/variable_Assignment.py
@@ -0,0 +1,12 @@
+# Assignment: Write a Python program that declares three variables: an integer, a float, and a string. Print these variables.
+
+# Declare and initialize variables
+an_integer = 24
+a_float = 3.1415
+a_string = "This is a string variable!"
+
+# Print the variables
+print(an_integer)
+print(a_float)
+print(a_string)
+
