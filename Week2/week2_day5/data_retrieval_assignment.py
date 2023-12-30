bc4816260e435ba0a3139fa7da4039885d226f7e
aluong15 <148726977+aluong15@users.noreply.github.com>
2023-12-29 16:07:18 -0800

    update with try except to data_retrieval_assignment

--- Week2/week2_day5/data_retrieval_assignment.py
+++ Week2/week2_day5/data_retrieval_assignment.py
@@ -8,29 +8,33 @@ user = "root"
 pw = "37952nsmyahooD!"
 db = "sm_app"
 
-# establish connection
-connection = mysql.connector.connect(
-    host=host, 
-    user=user, 
-    password=pw, 
-    database=db
-)
-print("Connection established")
+try:
+    # establish connection
+    connection = mysql.connector.connect(
+        host=host, 
+        user=user, 
+        password=pw, 
+        database=db
+    )
+    print("Connection established")
 
-# create cursor object
-cursor = connection.cursor()
-print("Cursor object created")
+    # create cursor object
+    cursor = connection.cursor()
+    print("Cursor object created")
 
-# select query
-query = "SELECT id, name, nationality FROM users"
-cursor.execute(query)
+    # select query
+    query = "SELECT id, name, nationality FROM users"
+    cursor.execute(query)
 
-result = cursor.fetchall()
+    result = cursor.fetchall()
 
-for x in result:
-    print(x)
+    for x in result:
+        print(x)
 
-# close connection
-if connection.is_connected():
-    connection.close()
-    print("Connection closed.")
+    # close connection
+    if connection.is_connected():
+        connection.close()
+        print("Connection closed.")
+
+except Error as err:
+    print('Error message: ' + err.msg)
